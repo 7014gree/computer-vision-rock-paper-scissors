9de9c4729e67206d7ceb31f7fa9d5efbd2f88eb6
7014gree <7014gree@gmail.com>
2022-11-06 03:20:06 +0000

    Camera working for snapshots, added text to frame, next to refactor to remove camera class, put Game instance within camera frame loop to feed in values to annotate on frame

--- camera_rps.py
+++ camera_rps.py
@@ -18,18 +18,20 @@ class Camera():
     def get_user_choice(self):
         ret, frame = self.cap.read()
         resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
-        cv2.imshow('frame', frame)
-        print(43)
-        cv2.waitKey(0)
-        print(44)
-        cv2.destroyAllWindows()
+        
         image_np = np.array(resized_frame)
         normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
         self.data[0] = normalized_image
         prediction = self.model.predict(self.data)
         print(prediction)
-        options = ["rock", "paper", "scissors", "Nothing"]
-        return options[np.argmax(prediction[0])]
+        choice = ["rock", "paper", "scissors", "Nothing"][np.argmax(prediction[0])]
+        cv2.putText(frame, choice, (50, 50), 0, 1.5, (250, 225, 120))
+        cv2.imshow('frame', frame)
+        print(43)
+        cv2.waitKey()
+        print(44)
+        cv2.destroyAllWindows()
+        return choice
     
 @dataclass
 class Game():
@@ -41,23 +43,6 @@ class Game():
     user_score = 0
 
     camera = Camera()
-
-    """@property
-    def computer_choice(self):
-        return self.computer_choice"""
-
-    """@computer_choice.setter
-    def computer_choice(self):
-        self.computer_choice = random.choice(["rock", "paper", "scissors"])"""
-
-    """@property
-    def user_choice(self):
-        return self.user_choice"""
-
-    """@user_choice.setter()
-    def user_choice(self):
-        print("Getting user choice from camera/model")
-        self.user_choice = 0"""
 
     def get_winner(self):
         print(self.computer_choice, self.user_choice)
